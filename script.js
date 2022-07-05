de181877425624ebd81dbbb9f6feebf7d753765c
Isaquee Braga <flaisaque@hotmail.com>
2022-07-05 12:43:12 -0300

    Estrutura de Dados

---
+++ script.js
@@ -0,0 +1,23 @@
+let Times = ["Flamengo", "Fluminense", "Vasco", "Palmeiras", "Botafogo"];
+console. log (Times);
+
+
+Times.unshift("Corinthias");
+console. log (Times);
+
+
+Times.pop();
+console. log(Times);
+
+Times.push("São Paulo", "Cruzeiro");
+console. log(Times);
+
+
+Times.shift();
+console. log(Times);
+
+
+let Numeros = [7,5,6,3,8,9,2,1,4];
+for(let i = 0; i <= 10; i ++){
+    console. log (i)
+}
